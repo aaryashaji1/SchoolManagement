24220a8d420dd4109883d8d267a7da82b1ab6a55
aaryashaji1 <aaryashajit@gmail.com>
2021-03-19 22:49:36 -0500

    Error testing

--- SchoolManagement/UserHome.Designer.cs
+++ SchoolManagement/UserHome.Designer.cs
@@ -29,137 +29,32 @@ namespace SchoolManagement
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTheme = new System.Windows.Forms.Label();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.lblMalayalam = new System.Windows.Forms.Label();
-            this.lblEnglish = new System.Windows.Forms.Label();
-            this.lblMaths = new System.Windows.Forms.Label();
-            this.lblTotal = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.btnLogOut = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblTheme
+            // button1
             // 
-            this.lblTheme.AutoSize = true;
-            this.lblTheme.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTheme.ForeColor = System.Drawing.Color.White;
-            this.lblTheme.Location = new System.Drawing.Point(611, 17);
-            this.lblTheme.Name = "lblTheme";
-            this.lblTheme.Size = new System.Drawing.Size(54, 18);
-            this.lblTheme.TabIndex = 0;
-            this.lblTheme.Text = "Theme";
-            this.lblTheme.Click += new System.EventHandler(this.lblTheme_Click);
-            // 
-            // lblUsername
-            // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.ForeColor = System.Drawing.Color.White;
-            this.lblUsername.Location = new System.Drawing.Point(38, 30);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(60, 24);
-            this.lblUsername.TabIndex = 1;
-            this.lblUsername.Text = "label2";
-            // 
-            // lblMalayalam
-            // 
-            this.lblMalayalam.AutoSize = true;
-            this.lblMalayalam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMalayalam.ForeColor = System.Drawing.Color.White;
-            this.lblMalayalam.Location = new System.Drawing.Point(38, 93);
-            this.lblMalayalam.Name = "lblMalayalam";
-            this.lblMalayalam.Size = new System.Drawing.Size(51, 20);
-            this.lblMalayalam.TabIndex = 2;
-            this.lblMalayalam.Text = "label2";
-            // 
-            // lblEnglish
-            // 
-            this.lblEnglish.AutoSize = true;
-            this.lblEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEnglish.ForeColor = System.Drawing.Color.White;
-            this.lblEnglish.Location = new System.Drawing.Point(38, 113);
-            this.lblEnglish.Name = "lblEnglish";
-            this.lblEnglish.Size = new System.Drawing.Size(51, 20);
-            this.lblEnglish.TabIndex = 3;
-            this.lblEnglish.Text = "label2";
-            // 
-            // lblMaths
-            // 
-            this.lblMaths.AutoSize = true;
-            this.lblMaths.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMaths.ForeColor = System.Drawing.Color.White;
-            this.lblMaths.Location = new System.Drawing.Point(38, 133);
-            this.lblMaths.Name = "lblMaths";
-            this.lblMaths.Size = new System.Drawing.Size(51, 20);
-            this.lblMaths.TabIndex = 4;
-            this.lblMaths.Text = "label2";
-            // 
-            // lblTotal
-            // 
-            this.lblTotal.AutoSize = true;
-            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.ForeColor = System.Drawing.Color.White;
-            this.lblTotal.Location = new System.Drawing.Point(38, 157);
-            this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(51, 20);
-            this.lblTotal.TabIndex = 5;
-            this.lblTotal.Text = "label2";
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.ForeColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(689, 57);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(87, 13);
-            this.linkLabel1.TabIndex = 6;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Create Password";
-            // 
-            // btnLogOut
-            // 
-            this.btnLogOut.Location = new System.Drawing.Point(692, 12);
-            this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(96, 30);
-            this.btnLogOut.TabIndex = 7;
-            this.btnLogOut.Text = "LogOut";
-            this.btnLogOut.UseVisualStyleBackColor = true;
-            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            this.button1.Location = new System.Drawing.Point(440, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // UserHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnLogOut);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.lblTotal);
-            this.Controls.Add(this.lblMaths);
-            this.Controls.Add(this.lblEnglish);
-            this.Controls.Add(this.lblMalayalam);
-            this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.lblTheme);
+            this.Controls.Add(this.button1);
             this.Name = "UserHome";
             this.Text = "UserHome";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserHome_FormClosing);
-            this.Load += new System.EventHandler(this.UserHome_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label lblTheme;
-        private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.Label lblMalayalam;
-        private System.Windows.Forms.Label lblEnglish;
-        private System.Windows.Forms.Label lblMaths;
-        private System.Windows.Forms.Label lblTotal;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button button1;
     }
 }
--- SchoolManagement/UserHome.cs
+++ SchoolManagement/UserHome.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,74 +12,11 @@ namespace SchoolManagement
 {
     public partial class UserHome : Form
     {
-        String connectionString = "Server=ARYASHAJI\\SQLEXPRESS;Database=SchoolManagement;Trusted_Connection=True;";
-        Student _student;
-        public UserHome(Student student)
+        public UserHome()
         {
             InitializeComponent();
-            _student = student;
         }
 
-        private void UserHome_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            UpdatewindowPosition(this.Left, this.Top, _student.Id);
-        }
-
-        private void UpdatewindowPosition(int left, int top,int studentid)
-        {
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-
-            SqlCommand command = new SqlCommand("UpdateWindowPosition", con);
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("Id", studentid);
-            command.Parameters.AddWithValue("Left", Left);
-            command.Parameters.AddWithValue("Top", Top);
-            command.ExecuteNonQuery();
-        }
-
-        private void UserHome_Load(object sender, EventArgs e)
-        {
-            this.BackColor = Color.FromArgb(Convert.ToInt32(_student.Color));
-            this.Left = _student.Left;
-            this.Top = _student.Top;
-            lblMalayalam.Text = "Malayalam :" + _student.Malayalam;
-            lblEnglish.Text = "English       :" + _student.English;
-            lblMaths.Text = "Maths         :" + _student.Maths;
-            lblTotal.Text = "Total           :" +( _student.Malayalam + _student.English + _student.Maths);
-            this.Text=lblUsername.Text = "Welcome....." + _student.Name;
-        }
-
-        private void lblTheme_Click(object sender, EventArgs e)
-        {
-            setColor(_student.Id);
-        }
-
-        private void setColor(int studentid)
-        {
-            colorDialog1.ShowDialog();
-            this.BackColor = Color.FromArgb(colorDialog1.Color.ToArgb());
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-
-            SqlCommand command = new SqlCommand("SetColor", con);
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("Id", studentid);
-            command.Parameters.AddWithValue("Color", colorDialog1.Color.ToArgb().ToString());
-            command.ExecuteNonQuery();
-            con.Close();
-        }
-
-        private void btnLogOut_Click(object sender, EventArgs e)
-        {
-            StudentLogin obj = new StudentLogin();
-            obj.Show();
-            this.Close();
-        }
-
-        //private void label1_Click(object sender, EventArgs e)
-        //{
-        //    Environment.Exit(0);
-        //}
+        
     }
 }
