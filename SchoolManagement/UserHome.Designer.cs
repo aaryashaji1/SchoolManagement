6a377122b4b4df6bec4d6c890eca718725b5d27e
aaryashaji1 <aaryashajit@gmail.com>
2021-03-19 23:42:48 -0500

    Third Commit

--- SchoolManagement/UserHome.Designer.cs
+++ SchoolManagement/UserHome.Designer.cs
@@ -29,32 +29,123 @@ namespace SchoolManagement
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblMalayalam = new System.Windows.Forms.Label();
+            this.lblEnglish = new System.Windows.Forms.Label();
+            this.lblMaths = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTheme = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnLogOut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // lblUsername
             // 
-            this.button1.Location = new System.Drawing.Point(440, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.White;
+            this.lblUsername.Location = new System.Drawing.Point(36, 37);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(45, 16);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "label1";
+            // 
+            // lblMalayalam
+            // 
+            this.lblMalayalam.AutoSize = true;
+            this.lblMalayalam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMalayalam.ForeColor = System.Drawing.Color.White;
+            this.lblMalayalam.Location = new System.Drawing.Point(36, 75);
+            this.lblMalayalam.Name = "lblMalayalam";
+            this.lblMalayalam.Size = new System.Drawing.Size(45, 16);
+            this.lblMalayalam.TabIndex = 1;
+            this.lblMalayalam.Text = "label1";
+            // 
+            // lblEnglish
+            // 
+            this.lblEnglish.AutoSize = true;
+            this.lblEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnglish.ForeColor = System.Drawing.Color.White;
+            this.lblEnglish.Location = new System.Drawing.Point(36, 97);
+            this.lblEnglish.Name = "lblEnglish";
+            this.lblEnglish.Size = new System.Drawing.Size(45, 16);
+            this.lblEnglish.TabIndex = 2;
+            this.lblEnglish.Text = "label1";
+            // 
+            // lblMaths
+            // 
+            this.lblMaths.AutoSize = true;
+            this.lblMaths.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaths.ForeColor = System.Drawing.Color.White;
+            this.lblMaths.Location = new System.Drawing.Point(36, 122);
+            this.lblMaths.Name = "lblMaths";
+            this.lblMaths.Size = new System.Drawing.Size(45, 16);
+            this.lblMaths.TabIndex = 3;
+            this.lblMaths.Text = "label1";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.White;
+            this.lblTotal.Location = new System.Drawing.Point(36, 147);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(45, 16);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "label1";
+            // 
+            // lblTheme
+            // 
+            this.lblTheme.AutoSize = true;
+            this.lblTheme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTheme.ForeColor = System.Drawing.Color.White;
+            this.lblTheme.Location = new System.Drawing.Point(606, 37);
+            this.lblTheme.Name = "lblTheme";
+            this.lblTheme.Size = new System.Drawing.Size(51, 16);
+            this.lblTheme.TabIndex = 5;
+            this.lblTheme.Text = "Theme";
+            this.lblTheme.Click += new System.EventHandler(this.lblTheme_Click);
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Location = new System.Drawing.Point(700, 29);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(88, 33);
+            this.btnLogOut.TabIndex = 6;
+            this.btnLogOut.Text = "LogOut";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
             // 
             // UserHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.lblTheme);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblMaths);
+            this.Controls.Add(this.lblEnglish);
+            this.Controls.Add(this.lblMalayalam);
+            this.Controls.Add(this.lblUsername);
             this.Name = "UserHome";
             this.Text = "UserHome";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserHome_FormClosing);
+            this.Load += new System.EventHandler(this.UserHome_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblMalayalam;
+        private System.Windows.Forms.Label lblEnglish;
+        private System.Windows.Forms.Label lblMaths;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblTheme;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnLogOut;
     }
 }
